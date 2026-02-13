6ea6ab62528688d931fdda34764866a27a76273a
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2026-02-13 17:03:57 +0200

    Update Vector.hpp

--- source/data_structures/vector/Vector.hpp
+++ source/data_structures/vector/Vector.hpp
@@ -1,37 +1,74 @@
-#include "String.h"
+#include <iostream>
 
-String::String() {
-    this->setSize(0);
+
+template <class T>
+class Vector {
+private:
+    T* data = nullptr;
+    size_t size = 0;
+    size_t capacity = 0;
+
+    void resize(size_t newCapacity);
+    void copyFrom(const Vector& other);
+    void moveTo(Vector&& other) noexcept;
+    void free();
+
+    void setSize(size_t newSize);
+    void setCapacity(size_t newCapacity);
+
+public:
+    Vector();
+    Vector(const Vector& other);
+    Vector(size_t capacity);
+    Vector(Vector&& other) noexcept;
+    Vector& operator = (const Vector& other);
+    Vector& operator = (Vector&& other) noexcept;
+    ~Vector();
+
+    void push(const T& element);
+    void push(T&& element);
+    void pop();
+
+    size_t getSize() const;
+    size_t getCapacity() const;
+
+    T& operator [] (size_t index);
+    const T& operator [] (size_t index) const;
+
+    template <class U>
+    friend std::ostream& operator << (std::ostream& os, const Vector<U>& vector);
+
+    template <class U>
+    friend std::istream& operator >> (std::istream& is, Vector<U>& vector);
+};
+
+
+template<class T>
+Vector<T>::Vector() {
     this->setCapacity(8);
-    this->data = new char[this->getCapacity()] {};
+    this->setSize(0);
+    this->data = new T[this->capacity] {};
 }
 
-String::String(const String& other) {
+template<class T>
+Vector<T>::Vector(const Vector &other) {
     copyFrom(other);
 }
 
-String::String(String&& other) noexcept {
+template<class T>
+Vector<T>::Vector(size_t capacity) {
+    this->setCapacity(capacity);
+    this->setSize(0);
+    this->data = new T[this->getCapacity()] {};
+}
+
+template<class T>
+Vector<T>::Vector(Vector &&other) noexcept {
     moveTo(std::move(other));
 }
 
-String::String(const char* data) {
-    if (!data) {
-        throw std::logic_error("Nullptr detected");
-    }
-
-    this->setSize(strlen(data));
-    this->setCapacity(this->getCapacity() * 2);
-    this->data = new char[this->getSize() + 1];
-    strncpy(this->data, data, strlen(data));
-}
-
-String::String(size_t capacity) {
-    this->setSize(0);
-    this->setCapacity(capacity);
-    this->data = new char[this->getCapacity()];
-}
-
-String& String::operator=(const String& other) {
+template<class T>
+Vector<T> &Vector<T>::operator=(const Vector &other) {
     if (this != &other) {
         free();
         copyFrom(other);
@@ -39,7 +76,8 @@ String& String::operator=(const String& other) {
     return *this;
 }
 
-String& String::operator=(String&& other) noexcept {
+template<class T>
+Vector<T> &Vector<T>::operator=(Vector &&other) noexcept {
     if (this != &other) {
         free();
         moveTo(std::move(other));
@@ -47,114 +85,143 @@ String& String::operator=(String&& other) noexcept {
     return *this;
 }
 
-String& String::operator+=(const String& other) {
-    size_t overallSize = this->getSize() + other.getSize() + 1;
-    if (this->getCapacity() < overallSize) {
-        this->resize(overallSize * 2);
-    }
-
-    strncat(this->data, other.getData(), other.getSize());
-    return *this;
-}
-
-String& String::operator+=(String&& other) noexcept {
-    size_t overallSize = this->getSize() + other.getSize() + 1;
-    if (this->getCapacity() < overallSize) {
-        this->resize(overallSize * 2);
-    }
-
-    strncat(this->data, other.getData(), other.getSize());
-    this->data[this->getSize()] = '\0';
-    return *this;
-}
-
-String::~String() {
+template <class T>
+Vector<T>::~Vector() {
     free();
 }
 
-void String::resize(size_t newCapacity) {
-    if (newCapacity <= this->getCapacity()) {
-        throw std::logic_error("NewCapacity must be greater");
+template<class T>
+void Vector<T>::resize(size_t newCapacity) {
+    if (newCapacity <= this->capacity) {
+        throw std::runtime_error("NewCapacity must be greater");
     }
 
-    char* newData = new char[newCapacity] {};
+    T* newData = new T[newCapacity] {};
     this->setCapacity(newCapacity);
-    strncpy(newData, data, this->getSize());
-    newData[this->getSize()] = '\0';
 
-    delete[] this->data;
-    this->data = newData;
+    for (int i = 0; i < this->size; i++) {
+        newData[i] = this->data[i];
+    }
 }
 
-void String::copyFrom(const String& other) {
-    this->data = new char[other.getCapacity()] {};
+template<class T>
+void Vector<T>::copyFrom(const Vector &other) {
+    this->data = new T[other.getCapacity()] {};
     this->setSize(other.getSize());
     this->setCapacity(other.getCapacity());
 
-    strncpy(this->data, other.data, other.getSize());
-    this->data[this->getSize()] = '\0';
+    for (int i = 0; i < this->size; i++) {
+        this->data[i] = other.data[i];
+    }
 }
 
-void String::moveTo(String&& other) noexcept {
+template<class T>
+void Vector<T>::moveTo(Vector &&other) noexcept {
     this->data = other.data;
-    this->setCapacity(other.getCapacity());
     this->setSize(other.getSize());
+    this->setCapacity(other.getCapacity());
 
     other.data = nullptr;
-    other.setSize(0);
     other.setCapacity(0);
+    other.setSize(0);
 }
 
-void String::free() {
+template<class T>
+void Vector<T>::free() {
     delete[] this->data;
     this->data = nullptr;
     this->setSize(0);
     this->setCapacity(0);
 }
 
-void String::setSize(size_t newSize) {
+template<class T>
+void Vector<T>::setSize(size_t newSize) {
     this->size = newSize;
 }
 
-void String::setCapacity(size_t newCapacity) {
+template<class T>
+void Vector<T>::setCapacity(size_t newCapacity) {
     this->capacity = newCapacity;
 }
 
-size_t String::getCapacity() const {
+template<class T>
+void Vector<T>::push(const T &element) {
+    if (this->getSize() == this->getCapacity()) {
+        this->resize(getCapacity() * 2);
+    }
+    this->data[this->getSize()] = element;
+    this->setSize(this->getSize() + 1);
+}
+
+template<class T>
+void Vector<T>::push(T &&element) {
+    if (this->getSize() == this->getCapacity()) {
+        this->resize(getCapacity() * 2);
+    }
+    this->data[this->getSize()] = std::move(element);
+    this->setSize(this->getSize() + 1);
+}
+
+template<class T>
+void Vector<T>::pop() {
+    if (this->getSize() == 0) {
+        throw std::logic_error("Thera are not any elements in the vector");
+    }
+    this->setSize(this->getSize() - 1);
+}
+
+template<class T>
+size_t Vector<T>::getCapacity() const {
     return this->capacity;
 }
 
-size_t String::getSize() const {
+template<class T>
+size_t Vector<T>::getSize() const {
     return this->size;
 }
 
-const char* String::getData() const {
-    return this->data;
-}
-
-char& String::operator[](size_t index) {
-    if (index >= this->getSize()) {
-        throw std::out_of_range("Index is out of range");
+template<class T>
+T &Vector<T>::operator[](size_t index) {
+    if (index > this->getSize()) {
+        throw std::logic_error("Out of range");
     }
     return this->data[index];
 }
 
-const char& String::operator[](size_t index) const {
-    if (index >= this->getSize()) {
-        throw std::out_of_range("Index is out of range");
+template<class T>
+const T &Vector<T>::operator[](size_t index) const {
+    if (index > this->getSize()) {
+        throw std::logic_error("Out of range");
     }
     return this->data[index];
 }
 
-std::ostream& operator << (std::ostream& os, const String& str) {
-    os << str.getData();
+template <class T>
+std::ostream& operator << (std::ostream& os, const Vector<T>& vector) {
+    size_t sizeOfVector = vector.getSize();
+
+    for (int i = 0; i < (int)sizeOfVector; ++i) {
+        os << vector[i];
+        if (i != sizeOfVector - 1) os << " ";
+    }
+
     return os;
 }
 
-std::istream& operator >> (std::istream& is, String& str) {
-    char buffer[1024] {};
-    is.read((char*)buffer, sizeof(buffer));
-    
+template <class T>
+std::istream& operator >> (std::istream& is, Vector<T>& vector) {
+    size_t sizeOfVector = 0;
+    size_t capacityOfVector = 0;
+    is >> sizeOfVector;
+    capacityOfVector = sizeOfVector * 2;
+
+    vector.free();
+    vector.data = new T[capacityOfVector] {};
+    vector.setSize(sizeOfVector);
+    vector.setCapacity(capacityOfVector);
+    for (int i = 0; i < sizeOfVector; i++) {
+        is >> vector.data[i];
+    }
 
     return is;
 }
