c074929e4bd4ab6a7793557dc75165e2fb93430b
b4ich3v <yoanbaychev@gmail.com>
2026-02-16 15:37:14 +0200

    Updated

--- source/sigma_algebra/SigmaAlgebra.cpp
+++ source/sigma_algebra/SigmaAlgebra.cpp
@@ -1,11 +1,17 @@
 #include "SigmaAlgebra.h"
+#include "source/sigma_algebra/sigma_algebra_factory/SigmaAlgebraFactory.h"
 
-SigmaAlgebra::SigmaAlgebra(const Omega& event, SigmaAlgebraPattern pattern) {
-    this->constructSigmaAlgebra();
+SigmaAlgebra::SigmaAlgebra(Omega* omega, SigmaAlgebraPattern pattern) {
+    this->setOmegaRef(omega);
+    this->setSigmaAlgebraPattern(pattern);
 }
 
-void SigmaAlgebra::constructSigmaAlgebra() {
-    this->addEmptyEvent();
-    this->addTheAddition();
-    this->addTheUnionOfAddition();
+void SigmaAlgebra::setOmegaRef(Omega* omega) {
+    if (omega == nullptr) throw std::logic_error("Nullptr error");
+    this->omega = omega;
+}
+
+void SigmaAlgebra::setSigmaAlgebraPattern(SigmaAlgebraPattern pattern) {
+    if (pattern == SigmaAlgebraPattern::NONE) throw std::logic_error("Not selected pattern");
+    this->pattern = pattern;
 }
--- source/sigma_algebra/SigmaAlgebra.h
+++ source/sigma_algebra/SigmaAlgebra.h
@@ -1,19 +1,25 @@
 #include "source/events/set_of_elementary_events/omega/Omega.h"
 #include "source/events/set_of_elementary_events/event/Event.h"
 #include "source/sigma_algebra/SigmaAlgebraPattern.h"
+#include "source/sigma_algebra/sigma_algebra_factory/SigmaAlgebraAbstractFactory.h"
 
 
 class SigmaAlgebra {
 private:
-    Vector<Event> containerOfEvents;
-    SigmaAlgebraPattern pattern;
-    // to be done: implement different consructing patterns for sigma-algebra
+    Omega* omega = nullptr;
+    SigmaAlgebraPattern pattern = SigmaAlgebraPattern::NONE;
 
     void addEmptyEvent();
     void addTheAddition();
     void addTheUnionOfAddition();
-    void constructSigmaAlgebra(); 
+    
+    void setOmegaRef(Omega* omega);
+    void setSigmaAlgebraPattern(SigmaAlgebraPattern pattern);
 
 public:
-    SigmaAlgebra(const Omega& event, SigmaAlgebraPattern pattern);
+    SigmaAlgebra(Omega* omega, SigmaAlgebraPattern pattern);
+    const Vector<Event> getContainerOfEvents() const;
+    
+    friend std::ostream& operator << (std::ostream& os, const SigmaAlgebra& sigmaAlgebra);
+    friend std::istream& operator >> (std::istream& is, SigmaAlgebra& sigmaAlgebra);
 };
