db3e27cb1b154e8b4c6e1d0d273b4acf4fce0d7e
b4ich3v <yoanbaychev@gmail.com>
2026-02-21 17:45:43 +0200

    Far better interface for function asbtract class

---
+++ source/functions/function/Function.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "source/data_structures/vector/Vector.hpp"
+
+template <class T>
+using Domain = Vector<T>;
+
+template <class T>
+using Range = Vector<T>;
+
+
+template<class T, class U>
+class Function {
+private:
+    Domain<T> domain;
+    Range<U> range;
+
+public:
+    Function(const Domain<T>& domain, const Range<U>& range);
+
+    virtual U operator () (T input) const = 0;
+    virtual Function* clone() const = 0;
+    virtual ~Function() = default;
+};
+
+template <class T, class U>
+Function<T, U>::Function(const Domain<T>& domain, const Range<U>& range):
+    domain(domain), range(range) {}
