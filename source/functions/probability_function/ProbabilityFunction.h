ab1c980e2aced686cf8eddc0af5412f17471913b
b4ich3v <yoanbaychev@gmail.com>
2026-02-21 18:33:39 +0200

    Header for prob function

---
+++ source/functions/probability_function/ProbabilityFunction.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "source/functions/function/Function.hpp" 
+#include "source/sigma_algebra/SigmaAlgebra.h"
+#include "source/events/set_of_elementary_events/event/Event.h"
+#include "source/data_structures/pair/Pair.hpp"
+#include "source/data_structures/vector/Vector.hpp"
+
+typedef double (*ProbabilityFunctionFilter)(const Event& event);
+
+
+class ProbabilityFunction: public Function<Event, double> {
+private:
+    const SigmaAlgebra* sigmaAlgebra = nullptr;
+    Vector<Pair<Event, double>> probabilities;
+
+    void fillProbabilities();
+    void setSigmaAlgebraPointer();
+
+public:
+    ProbabilityFunction(const SigmaAlgebra* sigmaAlgebra, ProbabilityFunctionFilter filter);
+
+    const SigmaAlgebra* getSigmaAlgebraPointer() const;
+    const Vector<Pair<Event, double>>& getProbabilities() const;
+
+    double operator () (const Event& input) const override;
+    Function* clone() const override;
+};
