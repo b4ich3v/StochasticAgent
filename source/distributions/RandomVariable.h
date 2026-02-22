f642034660bc4434509ea2b866b44a45596a408a
b4ich3v <yoanbaychev@gmail.com>
2026-02-22 16:15:25 +0200

    const reference to passed by value (copied)

--- source/distributions/RandomVariable.h
+++ source/distributions/RandomVariable.h
@@ -7,7 +7,7 @@
 template <class T>
 class RandomVariable {
 public:
-    virtual double calculateProbability(const T& number) const = 0;
+    virtual double calculateProbability(T number) const = 0;
     virtual double getExpectation() const = 0;
     virtual double getVariance() const = 0;
     virtual TypeOfRandomVariable getKind() const = 0;
