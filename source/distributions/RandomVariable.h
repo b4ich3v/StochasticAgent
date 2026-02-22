91bc54666c7739edc07c1185667bffac070c3644
b4ich3v <yoanbaychev@gmail.com>
2026-02-22 16:09:57 +0200

    Added method for calculating probabiity to be sent do current number on real path line

--- source/distributions/RandomVariable.h
+++ source/distributions/RandomVariable.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <iostream>
 #include "source/distributions/TypeOfRandomVariable.h"
 
 
 class RandomVariable {
 public:
+    virtual double calculateProbability(int32_t number) const = 0;
     virtual double getExpectation() const = 0;
     virtual double getVariance() const = 0;
     virtual TypeOfRandomVariable getKind() const = 0;
