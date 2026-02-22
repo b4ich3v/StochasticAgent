2c4e6503f3fcdd7dadc866795d58395d08d097d7
b4ich3v <yoanbaychev@gmail.com>
2026-02-22 20:15:52 +0200

    Fixed issue

--- source/data_structures/combinatorics/factoriel/Factoriel.cpp
+++ source/data_structures/combinatorics/factoriel/Factoriel.cpp
@@ -2,5 +2,5 @@
 
 size_t Factoriel::operator () (size_t number) const {
     if (number == 1 || number == 0) return 1;
-    return this->operator()(number - 1);
+    return number * this->operator()(number - 1);
 }
