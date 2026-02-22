c486e65f46331a97d00614c15a3855d7cea0f863
b4ich3v <yoanbaychev@gmail.com>
2026-02-22 16:50:20 +0200

    Implemented Bernoulli class

---
+++ source/distributions/discrete_distributions/bernoulii/Bernoulli.cpp
@@ -0,0 +1,21 @@
+#include "source/distributions/discrete_distributions/bernoulii/Bernoulli.h"
+
+Bernoulli::Bernoulli(Success success): RandomVariable(success, TypeOfRandomVariable::DISCRETE) {}
+
+double Bernoulli::calculateProbability(bool number) const {
+    if (number == 1) return this->getSuccessRate();
+    else if (number == 0) return this->getFailureRate();
+    else throw std::logic_error("Bolean must be 0 or 1");
+}
+
+double Bernoulli::getExpectation() const {
+    return this->getSuccessRate();
+}
+
+double Bernoulli::getVariance() const {
+    return this->getFailureRate() * this->getSuccessRate();
+}
+
+RandomVariable<bool>* Bernoulli::clone() const {
+    return new Bernoulli(*this);
+}
--- source/distributions/discrete_distributions/bernoulii/Bernoulli.h
+++ source/distributions/discrete_distributions/bernoulii/Bernoulli.h
@@ -1,2 +1,14 @@
 #pragma once
 
+#include "source/distributions/RandomVariable.hpp"
+
+
+class Bernoulli: public RandomVariable<bool> {
+public:
+    Bernoulli(Success success);
+
+    double calculateProbability(bool number) const override;
+    double getExpectation() const override;
+    double getVariance() const override;
+    RandomVariable* clone() const override;
+};
