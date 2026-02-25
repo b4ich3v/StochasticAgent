c9be508daa24a11a2e047b801f3ca00b6414671f
b4ich3v <yoanbaychev@gmail.com>
2026-02-25 14:20:07 +0200

    New constants

--- source/Constants.h
+++ source/Constants.h
@@ -1,5 +1,8 @@
 #pragma once
 
+static const char* RED = "\x1b[31m";
+static const char* RESET = "\x1b[0m";
+
 static constexpr int COUNT_OF_ELEMENTS_IN_ONE_BUCKET = 8;
 static constexpr int MAX_TOLERANCE_BITSET = 10000;
 static constexpr double EULER_NUMBER = 2.71828182846;
