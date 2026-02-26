698b806df17749577459c03ad3be044b828b8a54
b4ich3v <yoanbaychev@gmail.com>
2026-02-26 15:51:45 +0200

    Updated

--- source/functions/conditional_probability_function/ConditionalProbabilityFunction.h
+++ source/functions/conditional_probability_function/ConditionalProbabilityFunction.h
@@ -3,7 +3,7 @@
 #include "source/functions/probability_function/ProbabilityFunction.h"
 
 
-class ConditionalProbabilityFunction: ProbabilityFunction {
+class ConditionalProbabilityFunction: public ProbabilityFunction {
 public:
     ConditionalProbabilityFunction(const SigmaAlgebra* sigmaAlgebra, ProbabilityFunctionFilter filter);
     double operator () (const Event& left, const Event& right) const;
