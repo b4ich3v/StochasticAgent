3b2ffa1870307d1d6e5ee0e0e3b5da4134304c9f
b4ich3v <yoanbaychev@gmail.com>
2026-02-26 15:56:52 +0200

    Implemented logic for calculating full probability of event via full group of events of one omega set

---
+++ source/functions/conditional_probability_function/full_probability_formula/FullProbabilityFormula.cpp
@@ -0,0 +1,18 @@
+#include "source/functions/conditional_probability_function/full_probability_formula/FullProbabilityFormula.h"
+#include <stdexcept>
+
+FullProbabilityFormula::FullProbabilityFormula(const Event& event): event(event) {}
+
+double FullProbabilityFormula::operator()(const Vector<Event>& hypothesis, 
+    const ProbabilityFunction& probabilityFunction, const ConditionalProbabilityFunction& conditionalProbabilityFunction) const {
+    if (hypothesis.getSize() == 0) throw std::logic_error("Full probability formula requires a non-empty hypothesis set");
+
+    double result = 0.0;
+    for (size_t i = 0; i < hypothesis.getSize(); i++) {
+        const Event& h = hypothesis[i];
+        double pH = probabilityFunction(h);
+        double pAgivenH = conditionalProbabilityFunction(this->event, h);
+        result += pAgivenH * pH;
+    }
+    return result;
+}
---
+++ source/functions/conditional_probability_function/full_probability_formula/FullProbabilityFormula.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "source/events/set_of_elementary_events/event/Event.h"
+#include "source/functions/conditional_probability_function/ConditionalProbabilityFunction.h"
+#include "source/functions/probability_function/ProbabilityFunction.h"
+#include "source/data_structures/vector/Vector.hpp"
+
+
+class FullProbabilityFormula {
+private:
+    Event event;
+
+public:
+    FullProbabilityFormula(const Event& event);
+    double operator()(const Vector<Event>& hypothesis, 
+        const ProbabilityFunction& probabilityFunction, 
+        const ConditionalProbabilityFunction& conditionalProbabilityFunction) const;
+};
