88858d3050da0f61ed4a534df5daf81689b98358
b4ich3v <yoanbaychev@gmail.com>
2026-02-26 15:47:13 +0200

    Updated

--- source/independence/events_independence/EventsIndependence.cpp
+++ source/independence/events_independence/EventsIndependence.cpp
@@ -1,9 +1,16 @@
 #include "source/independence/events_independence/EventsIndependence.h"
 #include "source/functions/probability_function/ProbabilityFunction.h"
+#include "source/Constants.h"
+#include <cmath>
+#include <stdexcept>
 
 bool EventsIndependence::operator () (const Event& left, const Event& right, 
     Function<Event, double>* probabilityFunction) const {
-    return probabilityFunction->operator()(left) * 
-        probabilityFunction->operator()(right) == 
-        probabilityFunction->operator()(left & right);
+    if (!probabilityFunction) throw std::logic_error("Probability function pointer is null");
+
+    double pLeft = probabilityFunction->operator()(left);
+    double pRight = probabilityFunction->operator()(right);
+    double pIntersection = probabilityFunction->operator()(left & right);
+
+    return std::fabs(pLeft * pRight - pIntersection) <= EPS;
 }
