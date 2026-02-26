9da295d78d0526a34546cbf2c05a49a32944137a
b4ich3v <yoanbaychev@gmail.com>
2026-02-26 15:45:11 +0200

    Implemented FullGroupOfEvents class

---
+++ source/events/set_of_elementary_events/full_group_of_events/FullGroupOfEvents.cpp
@@ -0,0 +1,51 @@
+#include "source/events/set_of_elementary_events/full_group_of_events/FullGroupOfEvents.h"
+#include <stdexcept>
+
+FullGroupOfEvents::FullGroupOfEvents(const Vector<Event>& hypothesis, const Omega* omega) {
+    this->setOmegaRef(omega);
+    this->setHypothesis(hypothesis);
+}
+
+void FullGroupOfEvents::setOmegaRef(const Omega* omega) {
+    if (!omega) throw std::runtime_error("Nullptr detected");
+    this->omega = omega;
+}
+
+void FullGroupOfEvents::setHypothesis(const Vector<Event>& hypothesis) {
+    this->hypothesis = hypothesis;
+}
+
+bool FullGroupOfEvents::areIndependent() const {
+    for (size_t i = 0; i < hypothesis.getSize(); i++) {
+        for (size_t j = i + 1; j < hypothesis.getSize(); j++) {
+            Event intersection = hypothesis[i] & hypothesis[j];
+            if (intersection.getElementaryEvents().getSize() != 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool FullGroupOfEvents::areTheyFillOmege() const {
+    if (!omega) {
+        throw std::logic_error("Omega pointer is null");
+    }
+
+    BitSet coverage;
+    const Vector<ElementaryEvent>& omegaEvents = omega->getElementaryEvents();
+    for (size_t i = 0; i < hypothesis.getSize(); i++) {
+        const Vector<ElementaryEvent>& elems = hypothesis[i].getElementaryEvents();
+        for (size_t j = 0; j < elems.getSize(); j++) {
+            coverage.addNumber(elems[j].getEventId());
+        }
+    }
+
+    for (size_t i = 0; i < omegaEvents.getSize(); i++) {
+        if (!coverage.hasNumber(omegaEvents[i].getEventId())) {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ source/events/set_of_elementary_events/full_group_of_events/FullGroupOfEvents.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "source/events/set_of_elementary_events/event/Event.h"
+#include "source/events/set_of_elementary_events/omega/Omega.h"
+#include "source/independence/events_independence/TotalityEventsIndependence.h"
+
+
+class FullGroupOfEvents {
+private:
+    const Omega* omega = nullptr;
+    Vector<Event> hypothesis;
+
+    bool areIndependent() const;
+    bool areTheyFillOmege() const;
+    void setOmegaRef(const Omega* omega);
+    void setHypothesis(const Vector<Event>& hypothesis);
+  
+public:
+    explicit FullGroupOfEvents(const Vector<Event>& hypothesis, const Omega* omega);
+
+    const Event& operator [] (size_t index) const;
+    Event& operator [] (size_t index);
+};
