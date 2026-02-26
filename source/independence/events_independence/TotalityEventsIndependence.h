a4824b5414ba4b3f25f33c9b872f1e15351855f1
b4ich3v <yoanbaychev@gmail.com>
2026-02-26 15:47:36 +0200

    Added new import

--- source/independence/events_independence/TotalityEventsIndependence.h
+++ source/independence/events_independence/TotalityEventsIndependence.h
@@ -2,6 +2,7 @@
 
 #include "source/data_structures/vector/Vector.hpp"
 #include "source/events/set_of_elementary_events/event/Event.h"
+#include "source/functions/function/Function.hpp"
 
 
 class TotalityEventsIndependence {
