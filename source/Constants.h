291de6df6f07070e5a40576eb12cffd1c226db1e
b4ich3v <yoanbaychev@gmail.com>
2026-02-26 15:55:39 +0200

    Added new constant for comparing different doubles

--- source/Constants.h
+++ source/Constants.h
@@ -3,6 +3,7 @@
 static const char* RED = "\x1b[31m";
 static const char* RESET = "\x1b[0m";
 
+static constexpr double EPS = 1e-9;
 static constexpr int COUNT_OF_ELEMENTS_IN_ONE_BUCKET = 8;
 static constexpr int MAX_TOLERANCE_BITSET = 10000;
 static constexpr double EULER_NUMBER = 2.71828182846;
