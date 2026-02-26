85edb4a4f6f9858f2c38b6a2f4f91da0ae1c1149
b4ich3v <yoanbaychev@gmail.com>
2026-02-26 18:29:46 +0200

    Implemented alias

---
+++ source/functions/density_function/DensityFunction.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "source/functions/function/Function.hpp"
+#include "source/data_structures/interval/Interval.h"
+#include "source/data_structures/pair/Pair.hpp"
+
+
+class DensityFunction: public Function<double, double> {};
