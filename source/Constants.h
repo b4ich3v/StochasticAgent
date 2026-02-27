14410a568b8ba0bb83e6abba27ef185f9781996a
b4ich3v <yoanbaychev@gmail.com>
2026-02-27 23:28:33 +0200

    Added new constant

--- source/Constants.h
+++ source/Constants.h
@@ -7,3 +7,4 @@ static constexpr double EPS = 1e-9;
 static constexpr int COUNT_OF_ELEMENTS_IN_ONE_BUCKET = 8;
 static constexpr int MAX_TOLERANCE_BITSET = 10000;
 static constexpr double EULER_NUMBER = 2.71828182846;
+static constexpr double PI = 3.14159265359;
